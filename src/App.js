0932abdac46df90e46b68bbd07bc1bddc1a2d86b
pushker <pushker.tiwari@smartdatainc.net>
2019-02-26 18:42:01 +0530

    setup the react with material

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import IconButton from '@material-ui/core/IconButton';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import Bd from './utils/button.jsx';
+
+
+class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      value: ''
+    }
+  }
+  learnReact = () => {
+    this.setState({
+      value: "value"
+    })
+  }
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <IconButton aria-label="Delete">
+            <SvgIcon>
+              <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z" />
+            </SvgIcon>
+          </IconButton>
+          <Bd onClick={this.learnReact} textprops={'BUTTON'} href={'https://facebook.github.io/create-react-app'} />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/utils/button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from '@material-ui/core/Link';
+
+export default function ButtonLink(props) {
+    return (
+        <Link
+            component="button"
+            href={props.href}
+            target="_blank"
+            onClick={() => { props.onClick('e.target.value'); }}
+        >
+            {props.textprops}
+        </Link>
+    );
+}
